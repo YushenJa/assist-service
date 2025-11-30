2a03d0b321cdbda0a211fb64f8b2061f180ec4a6
yukorzho <yulia3224k@gmail.com>
2025-11-30 19:30:27 +0100

    test update

--- test/test-api.ts
+++ test/test-api.ts
@@ -2,7 +2,7 @@ async function testServer() {
     console.log ("Sende HTTP-Anfrage an lokalen Server...")
 
     try {
-        const response = await fetch('http://localhost:3000/chat', {
+        const response = await fetch('https://assist-service-251746638685.europe-west3.run.app/chat', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({message: "Hallo! Erzähl mir einen Witz über eine Katze"})
